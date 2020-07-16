c99335c8278d410b95584c376e248206ad5d3566
Lupo Solitario <duyluong1994@gmail.com>
2020-07-16 15:19:40 +0700

    u

--- chaincode/lixil_qrcode/src/schemas/user.ts
+++ chaincode/lixil_qrcode/src/schemas/user.ts
@@ -15,7 +15,43 @@ export const UserSchema = {
         }),
     firstName: yup.string().notRequired(),
     lastName: yup.string().notRequired(),
-    org: yup.object().shape(OrgSchema).notRequired(),
+    org: yup
+        .object()
+        .shape({
+            id: yup.string().required(),
+            name: yup.string().required(),
+            address: yup.string().notRequired(),
+            businessNumber: yup.string().notRequired(),
+            codePrefix: yup.string().notRequired(),
+            description: yup.string().notRequired(),
+            email: yup.string().email().notRequired(),
+            fax: yup.string().notRequired(),
+            hotLine: yup.string().notRequired(),
+            logoUrl: yup.string().url().notRequired(),
+            phone: yup.string().notRequired(),
+            representativeName: yup.string().notRequired(),
+            representativePosition: yup.string().notRequired(),
+            shortName: yup.string().notRequired(),
+            taxNumber: yup.string().notRequired(),
+            website: yup.string().url().notRequired(),
+            bizRoles: yup
+                .array()
+                .of(
+                    yup
+                        .string()
+                        .matches(/(PRODUCER|VERIFIER|TRANSPORTER|RETAILER)/)
+                )
+                .min(1)
+                .max(4)
+                .notRequired(),
+            media: yup
+                .array()
+                .of(yup.object().shape(MediaSchema))
+                .max(5)
+                .notRequired(),
+            establishedDate: yup.date().notRequired(),
+        })
+        .notRequired(),
     password: yup.string().notRequired(),
     phone: yup.string().notRequired(),
     position: yup.string().notRequired(),
