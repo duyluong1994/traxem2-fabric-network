a373747f652577ee5d6c3265caf7ee9c9ec3e5ce
Lupo Solitario <duyluong1994@gmail.com>
2020-10-12 16:20:35 +0700

    u

--- chaincode/lixil_qrcode/src/schemas/activity_log.ts
+++ chaincode/lixil_qrcode/src/schemas/activity_log.ts
@@ -18,15 +18,18 @@ export const ActivityLogSchema = yup.object().shape({
         ) {
             if (productionId) {
                 const context: any = this.options.context;
-                console.log(this.options, context);
+                console.log(context);
                 for (let id of productionId) {
-                    let dataAsBytes = await context.stub.getState(
+                    console.log(id);
+                    let dataAsBytes = await context.ctx.stub.getState(
                         PrefixMaster.PRODUCTION + id
                     );
+                    console.log(dataAsBytes);
                     if (!dataAsBytes || dataAsBytes.length === 0) {
                         return false;
                     }
                 }
+                return true;
             }
             return true;
         })
